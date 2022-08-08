354f3f7756859b96c722871fb484cdab56aa923a
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-08-09 00:52:54 +0700

    update

--- src/Containers/Dashboard/index.tsx
+++ src/Containers/Dashboard/index.tsx
@@ -1,6 +1,44 @@
-import React from 'react'
+import { Button } from 'antd'
+import React, { useEffect, useState } from 'react'
+import taskAPI from '../../api/Task'
 import LayoutComponent from '../../Components/Layout'
 const Dashboard: React.FC = () => {
-  return <LayoutComponent>Dashboard</LayoutComponent>
+  const [data, setData] = useState<any>([])
+  const handleGetAllTX = async () => {
+    try {
+      const res = await taskAPI.getAllTask()
+      setData(res?.data || [])
+    } catch (error) {
+
+    }
+  }
+  const handlePost = async (kq: any) => {
+    try {
+      const res: any = await taskAPI.addTask({
+        kq
+      })
+     await handleGetAllTX()
+    } catch (error) {
+
+    }
+  }
+  useEffect(() => {
+    handleGetAllTX()
+  }, [])
+  return <>
+    <div style={{maxWidth: '100%', width: '100%'}} >
+    {
+      data?.length > 0 && data.map((values: any) => {
+        return (
+          <span key={values.id} >{values.kq}</span>
+        )
+      })
+    }
+    </div>
+  <div>
+  <Button onClick={() => handlePost('tai,')} >Tai</Button>
+    <Button onClick={() => handlePost('xiu,')}  >Xiu</Button>
+  </div>
+  </>
 }
 export default Dashboard
--- src/api/Task/index.ts
+++ src/api/Task/index.ts
@@ -1,8 +1,8 @@
 import { ListResponse, Task, UpdateTaskProps } from "../../models";
 import axiosClient from "../axiosClient";
 const taskAPI = {
-  getAllTask(): Promise<ListResponse<Task>> {
-    const url = "/task";
+  getAllTask(): Promise<ListResponse<any>> {
+    const url = "/tx";
     return axiosClient.get(url);
     // return axiosClient.get(url, {
     //   params: {
@@ -11,8 +11,8 @@ const taskAPI = {
     //   },
     // });
   },
-  addTask(obj: Task): Promise<Task> {
-    const url = "/task";
+  addTask(obj: any): Promise<any> {
+    const url = "/tx";
     return axiosClient.post(url, obj);
     // return axiosClient.get(url, {
     //   params: {
